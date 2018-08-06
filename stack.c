8f986b6f4fe4cfea46a74dc4c52d561f9c4d3b50
vishal146 <41641720+vishal146@users.noreply.github.com>
2018-08-06 12:21:26 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<conio.h>
+
+int stk[100], top=-1, n;
+
+void show()         
+{
+     for(int i=0;i<=top;i++)
+     printf(“%d\t”,stk[i]);
+     printf(“\n”);
+ }
+ 
+void push()
+{
+     int item; 
+   
+     if(top == n-1)
+            printf(“\nStack is full.\n”);
+     else
+     {
+            printf(“\nEnter the item : “);
+            scanf(“%d”,&item);
+            stk[++top]=item;
+     }
+     show();
+}
+     
+void pop()
+{
+         
+         if(top==-1)
+             printf(“Stack is empty.”);
+         else
+         {
+             
+             printf(“%d is popped.”,stk[top]);
+             top–;
+         }
+}
+         
+int main()
+{
+          int i,op;
+          printf(“Enter the size of the stack : “);
+          scanf(“%d”,&n);                    
+          do
+          {
+             printf(“\n1 : Push”);
+             printf(“\n2 : Pop”);
+             printf(“\n3 : Print”);
+             printf(“\n4 : Exit”);
+             printf(“\nEnter your choice : “);
+             scanf(“%d”,&op);
+          
+             switch(op)
+             {
+                    case 1:
+                              push();
+                              break;
+                              
+                    case 2:
+                              pop();
+                              break;
+                              
+                    case 3:
+                              show();
+                              break;
+
+              }
+        }while(op!=4);
+        
+        printf(“\n—THE END—“);
+                           
+        getch();   
+}
